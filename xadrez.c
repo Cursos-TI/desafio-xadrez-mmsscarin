f1a6798e8945ade1b8e31109351d7e88ed12d97d
mmsscarin <mms.scarin@gmail.com>
2025-09-02 21:49:45 +0000

    Atualização para desafio Mestre

--- xadrez.c
+++ xadrez.c
@@ -1,45 +1,113 @@
 #include <stdio.h>
 
-int main (){
+//recursiva torre
+void torre(int i) {
+    if (i == 0) return;
+    printf ("Torre para direita\n");
+    torre(i - 1);
+         
+}
 
-    int i, j, k, l, m;
-
-    //movimento torre (5 casas para direita)
-    i = 1;
-
-    while (i <= 5) {
-        printf ("Torre para direita\n");
-        i++;
+//recursiva bispo
+void bispo (int j) {
+    if (j == 0) return;
+    
+    for (int i = 1; i <= 1; i++) {
+        
+        for (int k = 1; k <= 1; k++) {
+            printf ("Bispo para direita, ");
+                        
+        }
+        printf ("cima \n");
+                
+        
     }
     
+    bispo (j - 1);    
+    
+}
 
-    //movimento bispo (5 casas cima direita)
-    j = 1;
-    do
-    {
-            printf("Bispo para cima, direita\n");
-            j++;
-    }  while (j <= 5);
-        
-
-    //movimento rainha (8 casas esquerda)
-    for (k = 1; k <= 8; k++){
-        
+//recursiva rainha
+void rainha (int l) {
+    if (l > 0) {
         printf ("Rainha para esquerda\n");
-    }
 
-    //movimento cavalo (2 casas acima, 1 casa direita)
-    l = 1;
-    for ( l = 1; l <= 1; l++)  {
-                m = 1;
-        while (m <= 2)  {
-            printf ("Cavalo para cima\n");
-            m++;
+        rainha (l - 1);
+    }
+}
+
+//recursiva cavalo
+void cavalo (int m) {
+    if (m == 0) return;
+    
+    for (int i = 1; i <= 1; i++) {
+        int n = 3;
+        while (n > 0) {
+            if (n > 1) {
+                printf ("Cavalo para cima\n");
+                n--;
+                continue;
+            }
+            n--;
+            break;
+                        
         }
         printf ("Cavalo para direita\n");
-    }
-            
         
-    
-    return 0;
+               
+    }
+
+    cavalo (m - 1);
 }
+
+//funcao menu para facilitar manutencao do codigo e loop da opcao invalida
+void menu () {
+    printf ("Qual peça você quer movimentar?\n");
+    printf ("Escolha uma opção: \n");
+    printf ("1. Torre\n");
+    printf ("2. Bispo\n");
+    printf ("3. Rainha\n");
+    printf ("4. Cavalo\n");
+}
+
+int main (){
+
+    int opcao;
+    //quantidade de movimentos para cada peca
+    int qtorre = 5, qbispo = 5, qrainha = 8, qcavalo = 1;
+    
+    do {
+        menu ();
+        scanf ("%d", &opcao);
+
+        switch (opcao)
+        {
+        case 1:
+            //movimento torre (5 casas para direita)
+            torre (qtorre);
+            break;
+
+        case 2:
+            //movimento bispo (5 casas cima direita)
+            bispo (qbispo);
+            break;
+
+        case 3:
+            //movimento rainha (8 casas esquerda)
+            rainha (qrainha);
+            break;
+
+        case 4:
+            //movimento cavalo (2 casas acima, 1 casa direita)
+            cavalo(qcavalo);
+            break;
+
+        default:
+            printf ("Opção inválida. Tente outra vez.\n");
+        } 
+             
+    }
+    while (opcao <1 || opcao > 4);
+    //repeticao do menu para o jogador focar numa opcao valida   
+    return 0;
+}     
